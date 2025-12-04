3684ab7505a693437459592353efc387298fe9bd
Ильяс Герейханов <ilyas.gereykhanov@bk.ru>
2025-12-04 10:10:29 +0300

    добавлены роутеры для обновления и удаления питомцев

--- app/pets/routers.py
+++ app/pets/routers.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from app.pets.schemas import SPetBase, SPetCreate, SPetResponse, SPetUpdate
+from app.pets.schemas import SPetCreate, SPetUpdate, SPetResponse
 from app.pets.services import PetService
 
 router = APIRouter(prefix="/pets", tags=["Питомцы"])
@@ -18,5 +18,25 @@ async def get_pet(pet_id: int):
 
 @router.post("/", response_model=SPetResponse)
 async def create_pet(pet: SPetCreate):
-    new_pet = await PetService.create_pet(pet.dict())
+    new_pet = await PetService.create_pet(pet.model_dump())
     return new_pet
+
+@router.put("/{pet_id}", response_model=SPetResponse)
+async def update_pet(pet_id: int, pet: SPetUpdate):
+    pet_data = pet.model_dump(exclude_unset=True)
+
+    updated_pet = await PetService.update_pet(pet_id, pet_data)
+
+    if not updated_pet:
+        raise HTTPException(status_code=404, detail="Питомец не найден")
+
+    return updated_pet
+
+@router.delete("/{pet_id}")
+async def delete_pet(pet_id: int):
+    deleted = await PetService.delete_pet(pet_id)
+
+    if not deleted:
+        raise HTTPException(status_code=404, detail="Питомец не найден")
+
+    return {"detail": "Питомец удалён"}
--- app/pets/services.py
+++ app/pets/services.py
@@ -1,16 +1,46 @@
 from app.database import async_session
 from app.pets.models import Pet
-
+from sqlalchemy.future import select
 from app.base import BaseService
 
 class PetService(BaseService):
     model = Pet
-        
+
     @classmethod
-    async def create_pet(cls, pet_data):
+    async def create_pet(cls, pet_data: dict):
         async with async_session() as session:
             new_pet = Pet(**pet_data)
             session.add(new_pet)
             await session.commit()
             await session.refresh(new_pet)
             return new_pet
+
+    @classmethod
+    async def update_pet(cls, pet_id: int, pet_data: dict):
+        async with async_session() as session:
+            result = await session.execute(select(Pet).where(Pet.id == pet_id))
+            pet = result.scalar_one_or_none()
+
+            if not pet:
+                return None
+
+            for key, value in pet_data.items():
+                setattr(pet, key, value)
+
+            await session.commit()
+            await session.refresh(pet)
+
+            return pet
+
+    @classmethod
+    async def delete_pet(cls, pet_id: int):
+        async with async_session() as session:
+            result = await session.execute(select(Pet).where(Pet.id == pet_id))
+            pet = result.scalar_one_or_none()
+
+            if not pet:
+                return False
+
+            await session.delete(pet)
+            await session.commit()
+            return True
